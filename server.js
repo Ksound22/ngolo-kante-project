0933feb7e8a5715f48b7aaa1f8529e7e88eb05e2
ksound22 <chrisjay967@gmail.com>
2021-07-20 20:22:11 +0200

    Few changes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
 const cors = require("cors");
 require("dotenv").config();
 
@@ -20,6 +22,16 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1", jokeRoutes);
+
+// Serve static assets for deployment
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
